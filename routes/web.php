5125997227e6af10111202b0782e1724d51c1fa6
Михаил Перетятько <mihailperetatko@MacBook-Pro-Mihail.local>
2021-04-09 01:22:00 +0300

    Added route seller

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('pages.main.index');
 })->name('index');
+
+Route::get('/seller', function () {
+    return view('pages.main.seller');
+})->name('seller');
