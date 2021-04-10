980f8612335c047f97b777fb88cb917933643bf7
Владислав <dvovlad@mail.ru>
2021-04-10 14:16:55 +0300

    registration step 1 and save changes

---
+++ resources/views/components/user-field.blade.php
@@ -0,0 +1,5 @@
+<div class="form-group">
+	<label class="form-label" for="phone">Телефон</label>
+	<input class="form-input" id="phone" name="phone" type="{{ $type }}" placeholder="+70000000000" data-validate="require">
+</div>
+
