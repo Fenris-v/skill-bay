47a10ab024ea91af4a942c96b7c4d3aaea3a2d93
Roman Sarvarov <roman.sarvarov@gmail.com>
2021-04-12 19:55:34 +0300

    fix type

--- app/Services/ProductCartService.php
+++ app/Services/ProductCartService.php
@@ -4,7 +4,7 @@ namespace App\Services;
 
 use App\Models\Product;
 use Illuminate\Database\Eloquent\Collection;
-use \App\Contracts\ProductCartService as ProductCartServiceContract;
+use App\Contracts\ProductCartService as ProductCartServiceContract;
 
 /**
  * Класс-сервис для работы с корзиной товаров.
