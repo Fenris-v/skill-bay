2f6941e3f7c88aa8f2ac1e6a4fc2bb4c623f49f8
Владислав <dvovlad@mail.ru>
2021-04-19 15:45:04 +0300

    fix 2

--- resources/views/pages/forgot-password/reset-password.blade.php
+++ resources/views/pages/forgot-password/reset-password.blade.php
@@ -10,10 +10,10 @@
     <div class="Middle-top">
       <div class="wrap">
         <div class="Middle-header">
-          <h1 class="Middle-title">Авторизация</h1>
+          <h1 class="Middle-title">Сбросить пароль</h1>
           <ul class="breadcrumbs Middle-breadcrumbs">
             <li class="breadcrumbs-item"><a href="index.html">Главная</a></li>
-            <li class="breadcrumbs-item breadcrumbs-item_current"><span>Авторизация</span></li>
+            <li class="breadcrumbs-item breadcrumbs-item_current"><span>Сбросить пароль</span></li>
           </ul>
         </div>
       </div>
