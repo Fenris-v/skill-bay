dcd4c0ea0f3b89000b60cf10804ebba1e0b96cae
Владислав <dvovlad@mail.ru>
2021-04-22 11:26:30 +0300

    fix user field component

--- resources/views/components/user-field.blade.php
+++ resources/views/components/user-field.blade.php
@@ -1,5 +1,5 @@
 <div class="form-group">
-	<label class="form-label" for="phone">{{ $slot }}</label>
+	<label class="form-label" for="{{$attributes->get('id')}}">{{ $slot }}</label>
 	<input {{ $attributes }} class="form-input" data-validate="require">
 </div>
 
