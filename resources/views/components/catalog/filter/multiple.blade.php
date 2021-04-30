ba3b89002eabfa6622ff802001af655127d07db7
Anatoliy Vasilenko <anatolijv236@gmail.com>
2021-04-30 17:04:33 +0000

    fix filter

--- resources/views/components/catalog/filter/multiple.blade.php
+++ resources/views/components/catalog/filter/multiple.blade.php
@@ -1,12 +1,13 @@
 <div class="form-group">
-    <select class="form-select" multiple size="4"
-            name="filter[props][{{ $specification->slug }}][]">
-        <option disabled="disabled">{{ $specification->title }}</option>
-        <option value="">{{ __('catalog.not-selected') }}</option>
-        @foreach($specificationsValues as $value)
-            <option
-                    {{ $filtering && in_array($value, $filterValue) ? 'selected' : '' }}
-                    value="{{ $value->value }}">{{ $value->value }}</option>
-        @endforeach
-    </select>
+    <label>{{ $specification->title }}
+        <select class="form-select" multiple size="4"
+                name="filter[props][{{ $specification->slug }}][]">
+
+            <option>{{ __('catalog.not-selected') }}</option>
+            @foreach($specificationsValues as $value)
+                <option {{ $filtering && in_array($value->value, $filterValue) ? 'selected' : '' }}
+                        value="{{ $value->value }}">{{ $value->value }}</option>
+            @endforeach
+        </select>
+    </label>
 </div>
