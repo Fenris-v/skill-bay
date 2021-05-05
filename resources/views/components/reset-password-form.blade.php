45969438ecef55b8a8dcae564109fc0e3d1ceab1
Владислав <dvovlad@mail.ru>
2021-05-05 19:18:42 +0300

    add translate

--- resources/views/components/reset-password-form.blade.php
+++ resources/views/components/reset-password-form.blade.php
@@ -4,9 +4,9 @@
 	@include('includes.validate_status')
 	<div class="row">
 		<div class="row-block">
-			<x-user-field type="email"  name="email" title="Email" id="email" placeholder="{{__('user_messages.placeholder_mail')}}">Email</x-user-field>
-			<x-user-field type="password"  name="password" title="password" id="password" placeholder="{{__('user_messages.placeholder_password')}}">Password</x-user-field>
-			<x-user-field type="password"  name="password_confirmation" title="password confirmation" id="password_confirmation" placeholder="{{__('user_messages.placeholder_password_reply')}}">Confirm password</x-user-field>
+			<x-user-field type="email"  name="email" title="Email" id="email" placeholder="{{__('user_messages.placeholder_mail')}}">{{__('user_messages.email')}}</x-user-field>
+			<x-user-field type="password"  name="password" title="password" id="password" placeholder="{{__('user_messages.placeholder_password')}}">{{__('user_messages.password')}}</x-user-field>
+			<x-user-field type="password"  name="password_confirmation" title="password confirmation" id="password_confirmation" placeholder="{{__('user_messages.placeholder_password_reply')}}">{{__('user_messages.password_confirm')}}</x-user-field>
 			<input name="token" type="hidden" value="{{$token}}">
 			<div class="form-group">
 				<button class="btn btn_primary" type="submit">{{__('user_messages.reset_password')}}</button>
--- resources/views/layouts/header/auth.blade.php
+++ resources/views/layouts/header/auth.blade.php
@@ -2,11 +2,11 @@
 <div class="row ControlPanel-rowSplit">
     <div class="row-block">
 		@auth
-		<a class="ControlPanel-title" href="{{route('logout')}}">Выйти</a>
+		<a class="ControlPanel-title" href="{{route('logout')}}">{{__('user_messages.logout')}}</a>
 		@endauth
 		@guest
-        <a class="ControlPanel-title" href="{{route('registration')}}">Войти</a>&nbsp;/&nbsp;
-        <a class="ControlPanel-title" href="{{route('login')}}">Регистрация</a>
+        <a class="ControlPanel-title" href="{{route('login')}}">{{__('user_messages.login')}}</a>&nbsp;/&nbsp;
+        <a class="ControlPanel-title" href="{{route('registration')}}">{{__('user_messages.registation')}}</a>
 		@endguest
     </div>
 </div>
