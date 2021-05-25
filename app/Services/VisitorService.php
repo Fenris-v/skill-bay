dfc558e57b66b44ecfddfd8a00dd0823419ee5d3
Oleg Sviridov <olegsv3007@gmail.com>
2021-05-25 18:44:17 +0000

    use cache for visitor

--- app/Services/VisitorService.php
+++ app/Services/VisitorService.php
@@ -3,27 +3,42 @@
 namespace App\Services;
 
 use App\Models\Visitor;
+use App\Repository\ConfigRepository;
 use Illuminate\Http\Request;
 use Cookie;
+use Cache;
 
 class VisitorService
 {
+    private $configRepository;
+
+    public function __construct(ConfigRepository $configRepository) {
+        $this->configRepository = $configRepository;
+    }
 
     public function get()
     {
+        $ttl = $this->configRepository->getCacheLifetime(now()->addDay());
+
         if (auth()->check()) {
-            $visitor = auth()->user()->visitor ?? Visitor::create(['user_id' => auth()->id()]);
-
-        } else {
-            $visitorId = Cookie::get('visitor_id');
-
-            if (!($visitor =  Visitor::find($visitorId))) {
-                $visitor = Visitor::create();
-                Cookie::queue('visitor_id', $visitor->id, 3600);
-            }
+            return Cache::tags(Visitor::VISITOR_CACHE_TAGS)
+                ->remember('visitor_auth_' . auth()->id(), $ttl, function() {
+                    return auth()->user()->visitor ?? Visitor::create(['user_id' => auth()->id()]);
+                });
         }
 
-        return $visitor;
+        $visitorId = Cookie::get('visitor_id');
 
+        if (!$visitorId) {
+            $visitor = Visitor::create();
+            Cookie::queue('visitor_id', $visitor->id, 3600);
+            Cache::tags(Visitor::class)->set('visitor_guest_' . $visitor->id, $visitor, $ttl);
+            return $visitor;
+        }
+
+        return Cache::tags(Visitor::VISITOR_CACHE_TAGS)
+            ->remember('visitor_guest_' . $visitorId, $ttl, function() use ($visitorId) {
+                return Visitor::find($visitorId);
+            });
     }
 }
