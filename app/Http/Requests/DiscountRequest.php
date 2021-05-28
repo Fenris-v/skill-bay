d11c4fdf520046cafd54b32cba0fb948575281de
Михаил Перетятько <mihailperetatko@MacBook-Pro-Mihail.local>
2021-05-28 06:33:54 +0300

    Modified DiscountRequest rules for dates

--- app/Http/Requests/DiscountRequest.php
+++ app/Http/Requests/DiscountRequest.php
@@ -18,8 +18,8 @@ class DiscountRequest extends FormRequest
         return [
             'discount.title' => 'required|min:3|max:255',
             'discount.description' => 'required|min:3|max:65535',
-            'discount.begin_at' => 'date_format:Y-m-d|before:end_at',
-            'discount.end_at' => 'date_format:Y-m-d|after:begin_at',
+            'discount.begin_at' => 'nullable|date_format:Y-m-d|before:discount.end_at',
+            'discount.end_at' => 'nullable|date_format:Y-m-d|after:discount.begin_at',
             'discount.value' => 'required|numeric|min:0',
             'discount.unit_type' => [
                 Rule::in(array_keys(Discount::unitTypes())),
