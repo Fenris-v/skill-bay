0ecb286e22b7160aa8af5c0d7cef6408684d88b4
fenris <Fenris-v>
2021-05-29 18:13:23 +0300

    add breadcrumbs for auth pages. Add sender email to .env.example. Reformat code in UserController.php

--- resources/views/pages/registration/index.blade.php
+++ resources/views/pages/registration/index.blade.php
@@ -1,27 +1,16 @@
 {{--Основной шаблон страницы регистрации--}}
 @extends('layouts.layout')
 
-@section('title', 'Регистрация')
+@section('title', __('login.reg'))
 
-@section('meta_description', 'Описание страницы')
+@section('meta_description', __('login.desc'))
+
+@section('middle-header-h1', __('login.reg'))
 
 @section('content')
-<div class="Middle Middle_top">
-    <div class="Middle-top">
-      <div class="wrap">
-        <div class="Middle-header">
-          <h1 class="Middle-title">Регистрация</h1>
-          <ul class="breadcrumbs Middle-breadcrumbs">
-            <li class="breadcrumbs-item"><a href="/">Главная</a></li>
-            <li class="breadcrumbs-item breadcrumbs-item_current"><span>Регистрация</span></li>
-          </ul>
-        </div>
-      </div>
-    </div>
     <div class="Section">
-      <div class="wrap">
-		  <x-registration-form/>
-      </div>
+        <div class="wrap">
+            <x-registration-form/>
+        </div>
     </div>
-  </div>
 @endsection
